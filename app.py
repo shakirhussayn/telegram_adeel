c1c0033c0dd15f48066b99670a1883c443daf273
Muhammad Aaqib <123294864+shakirhussayn@users.noreply.github.com>
2025-08-10 13:39:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ async def photo_worker():
             daily_counter += 1
             history_counter += 1
             
-            delay = random.randint(7, 9)
+            delay = random.randint(5, 6)
             print(f"  -> Waiting for {delay} seconds before next photo...")
             await asyncio.sleep(delay)
         except Exception as e:
